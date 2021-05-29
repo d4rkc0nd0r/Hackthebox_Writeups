3cf1f93703c97be0017e63473edf0a575f33aaff
Siddharth Reddy <sidd_tim@outlook.com>
2021-05-29 16:15:05 +0530

    Update script.py

--- challenges/crypto/babyencryption/script.py
+++ challenges/crypto/babyencryption/script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def encryption(msg):
+def decryption(msg):
     ct = []
     for char in msg:
         ct.append((123 * ord(char) + 18) % 256)
@@ -10,7 +10,7 @@ a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789{}!@#$%^&*()_
 f = []
 
 for i in a:
-	ct = encryption(i)
+	ct = decryption(i)
 	f.append(ct.hex())
 
 l = ['aa', '6e', 'c8', '2b', 'f6', '22', '22', '7b', 'b7', '0e', '7f', 'b7', '35', '22', '49', 'b7', 'd8', '93', 'c4', '93', 'd8', '53', '9d', 'ec', '8f', 'b7', '93', '5d', '49', '0e', '7f', '9d', '22', 'ec', '89', 'b7', 'a3', '22', 'ec', '8f', 'd8', '0e', '7f', '89', '21']
